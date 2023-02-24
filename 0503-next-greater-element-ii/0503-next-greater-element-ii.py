4e52831e3017fc7a2d0f43d2c75a4c09cfb0427b
Biruk Kassaw <61546383+biruk-kassaw@users.noreply.github.com>
2023-02-24 15:47:45 +0300

    Time: 242 ms (40.77%), Space: 16.1 MB (17.30%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.py
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.py
@@ -2,14 +2,18 @@ class Solution:
     def nextGreaterElements(self, nums: List[int]) -> List[int]:
         stack = []
         next_greater = {}
-        n = len(nums)      
+        n = len(nums)   
+        ans = [-1] * len(nums)
+        
         for i in range(2 * n):
             while stack and nums[i%n] > stack[-1][1]:
-                next_greater[stack.pop()] = nums[i%n]
+                ans[stack.pop()[0]] = nums[i%n]
             stack.append((i%n,nums[i%n]))
             
-        ans = [-1] * len(nums)
-                
-        for num in next_greater:
-            ans[num[0]] = next_greater[num]
         return ans
+    
+    
+    
+    
+    
+    
