7ce88f3a53d89b10f8310cc6d93b0362a3818ab2
Biruk Kassaw <61546383+biruk-kassaw@users.noreply.github.com>
2023-04-10 18:43:51 +0300

    Time: 394 ms (19.72%), Space: 25.1 MB (11.49%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,15 +1,15 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
-        visited = [[False for i in range(len(grid[0]))] for j in range(len(grid))]
+        visited = set()
         def inbound(row, col):
             return 0 <= row < len(grid) and 0 <= col < len(grid[0])
         
         def dfs(row, col):
-            if not inbound(row, col) or visited[row][col] or grid[row][col] == "0":
+            if not inbound(row, col) or tuple([row, col]) in visited or grid[row][col] == "0":
                 return
             
-            visited[row][col] = True
+            visited.add(tuple([row, col]))
             for r, c in directions:
                 new_row = row + r
                 new_col = col + c
@@ -18,7 +18,7 @@ class Solution:
         count = 0
         for i in range(len(grid)):
             for j in range(len(grid[0])):
-                if grid[i][j] == "1" and not visited[i][j]:
+                if grid[i][j] == "1" and tuple([i, j]) not in visited:
                     count += 1
                     dfs(i, j)
                     
