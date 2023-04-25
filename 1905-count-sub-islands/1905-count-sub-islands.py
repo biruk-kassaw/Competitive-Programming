a691ed996fc7bc25073895b4b15e71c383adbbce
Biruk Kassaw <61546383+biruk-kassaw@users.noreply.github.com>
2023-04-25 22:41:46 +0300

    Time: 3792 ms (10.65%), Space: 110.2 MB (13.29%) - LeetHub

--- 1905-count-sub-islands/1905-count-sub-islands.py
+++ 1905-count-sub-islands/1905-count-sub-islands.py
@@ -4,26 +4,30 @@ class Solution:
             return 0 <= row < len(grid2) and 0 <= col < len(grid2[0])
         
         def dfs(row, col):
-            visited_l.add((row, col))
+            if not inbound(row, col) or (row,col) in visited or grid2[row][col] == 0:
+                return True
+            
             visited.add((row, col))
+            res =  True
+            
+            if grid1[row][col] != 1:
+                res = False
+                
             for dir_r, dir_c in directions:
                 new_row = dir_r + row
                 new_col = dir_c + col
-                if inbound(new_row, new_col) and (new_row,new_col) not in visited and grid2[new_row][new_col] == 1:
-                    dfs(new_row, new_col)
+#                 the order we call the function matters because and operator will shortcircuit if res is false
+                res =  dfs(new_row, new_col) and res
                     
+            return res
+        
         directions = [(1,0),(-1,0),(0,1),(0,-1)]
         visited = set()
         count = 0
         
         for i in range(len(grid2)):
             for j in range(len(grid2[0])):
-                if grid2[i][j] == 1 and (i, j) not in visited:
-                    visited_l  = set()
-                    dfs(i, j)
-                    is_sub = True
-                    for row, col in visited_l:
-                        if grid1[row][col] == 0:
-                            is_sub = False
-                    count += int(is_sub)
+                if grid2[i][j] == 1 and (i, j) not in visited and dfs(i,j):
+                    count += 1
+                    
         return count
