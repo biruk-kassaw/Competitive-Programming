f4adc9c4a43a7ff0e0a08c321ea8972138ba9bc1
Biruk Kassaw <61546383+biruk-kassaw@users.noreply.github.com>
2023-05-30 22:38:49 +0300

    Create 1137. N-th Tribonacci Number.py

---
+++ 1137. N-th Tribonacci Number.py
@@ -0,0 +1,6 @@
+class Solution:
+    memo = {0: 0, 1: 1, 2: 1}
+    def tribonacci(self, n: int) -> int:
+        if n not in self.memo:
+            self.memo[n] = self.tribonacci(n-1) + self.tribonacci(n-2) + self.tribonacci(n-3)
+        return self.memo[n]
