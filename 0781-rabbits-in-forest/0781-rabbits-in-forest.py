267623dbe36d361c003283427e5e9667ccae906d
Biruk Kassaw <61546383+biruk-kassaw@users.noreply.github.com>
2023-08-18 05:34:18 +0300

    Time: 50 ms (68.18%), Space: 16.5 MB (12.55%) - LeetHub

---
+++ 0781-rabbits-in-forest/0781-rabbits-in-forest.py
@@ -0,0 +1,11 @@
+class Solution:
+    def numRabbits(self, answers: List[int]) -> int:
+        c = Counter(answers)
+        res = 0
+        for key,val in c.items():
+            remaining = val % (key + 1)
+            if remaining == 0:
+                continue
+            res += key - remaining +1
+                
+        return res + len(answers)
